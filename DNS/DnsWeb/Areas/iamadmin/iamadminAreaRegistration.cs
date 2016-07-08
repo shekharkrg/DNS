5e45e7e2b92793df45ed70651b3c402d1264dd75
Shekhar Kumar <shekher.1989.kumar@gmail.com>
2016-07-08 07:25:41 +0530

    Initial Commit

---
+++ DNS/DnsWeb/Areas/iamadmin/iamadminAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace DnsWeb.Areas.iamadmin
+{
+    public class iamadminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "iamadmin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "iamadmin_default",
+                "iamadmin/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ DNS/DnsWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DnsWeb.Global" Language="C#" %>
