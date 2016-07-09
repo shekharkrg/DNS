ac34650591375e50bc14f9af415f53b56a24b94f
Shekhar Kumar <shekher.1989.kumar@gmail.com>
2016-07-09 21:53:53 +0530

    Modified ViewLogin type

--- DNS/DataBase/Entities/Viewer/ViewerLogin.cs
+++ DNS/DataBase/Entities/Viewer/ViewerLogin.cs
@@ -1,9 +1,15 @@
 ﻿namespace DataBase
 {
+    #region usings
+    using Shared;
+    #endregion
+
     public class ViewerLogin
     {
         public int ViewerLoginID { get; set; }
         public string ViewerName { get; set; }
         public string ViewerEmail { get; set; }
+        public string Avatar { get; set; }
+        public LoginType LoginType { get; set; }
     }
 }
---
+++ DNS/Shared/Enums/LoginType.cs
@@ -0,0 +1,11 @@
+﻿namespace Shared
+{
+    public enum LoginType : ushort
+    {
+        Email = 1,
+        Google,
+        Facebook,
+        LinkedIn,
+        Twitter
+    }
+}
